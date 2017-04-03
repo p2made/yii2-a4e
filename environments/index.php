05f8bc66a71abbea6aac305dd4c64ec024da9966
Pedro Plowman <p2opensource@gmail.com>
2017-04-03 20:34:07 +1000

    v0.0.7

--- environments/index.php
+++ environments/index.php
@@ -32,12 +32,6 @@ return [
 	'Development' => [
 		'path' => 'dev',
 		'setWritable' => [
-			'backend/runtime',
-			'backend/web/assets',
-			'frontend/runtime',
-			'frontend/web/assets',
-		],
-		'setWritable' => [
 			'api/controllers',
 			'api/models',
 			'api/runtime',
@@ -46,16 +40,21 @@ return [
 			'assets/models',
 			'assets/runtime',
 			'assets/views',
-			'assets/web/pub',
 			'backend/controllers',
 			'backend/models',
 			'backend/runtime',
 			'backend/views',
+			'common/controllers',
 			'common/models',
 			'frontend/controllers',
 			'frontend/models',
 			'frontend/runtime',
 			'frontend/views',
+			'assets/web/api/pub',
+			'assets/web/assets/pub',
+			'assets/web/backend/pub',
+			'assets/web/common/pub',
+			'assets/web/frontend/pub',
 		],
 		'setExecutable' => [
 			'yii',
@@ -73,9 +72,13 @@ return [
 		'setWritable' => [
 			'api/runtime',
 			'assets/runtime',
-			'assets/web/pub',
 			'backend/runtime',
 			'frontend/runtime',
+			'assets/web/api/pub',
+			'assets/web/assets/pub',
+			'assets/web/backend/pub',
+			'assets/web/common/pub',
+			'assets/web/frontend/pub',
 		],
 		'setExecutable' => [
 			'yii',
@@ -83,68 +86,6 @@ return [
 		'setCookieValidationKey' => [
 			'api/config/main-local.php',
 			'assets/config/main-local.php',
-			'backend/config/main-local.php',
-			'frontend/config/main-local.php',
-		],
-	],
-];
-?>
-
-
-<?php
-/**
- * The manifest of files that are local to specific environment.
- * This file returns a list of environments that the application
- * may be installed under. The returned data must be in the following
- * format:
- *
- * ```php
- * return [
- *	 'environment name' => [
- *		 'path' => 'directory storing the local files',
- *		 'skipFiles'  => [
- *			 // list of files that should only copied once and skipped if they already exist
- *		 ],
- *		 'setWritable' => [
- *			 // list of directories that should be set writable
- *		 ],
- *		 'setExecutable' => [
- *			 // list of files that should be set executable
- *		 ],
- *		 'setCookieValidationKey' => [
- *			 // list of config files that need to be inserted with automatically generated cookie validation keys
- *		 ],
- *		 'createSymlink' => [
- *			 // list of symlinks to be created. Keys are symlinks, and values are the targets.
- *		 ],
- *	 ],
- * ];
- * ```
- */
-return [
-	'Development' => [
-		'path' => 'dev',
-		'setExecutable' => [
-			'yii',
-			'yii_test',
-		],
-		'setCookieValidationKey' => [
-			'backend/config/main-local.php',
-			'frontend/config/main-local.php',
-		],
-	],
-	'Production' => [
-		'path' => 'prod',
-		'setWritable' => [
-			'backend/runtime',
-			'backend/web/assets',
-			'frontend/runtime',
-			'frontend/web/assets',
-		],
-		'setExecutable' => [
-			'yii',
-		],
-		'setCookieValidationKey' => [
 			'backend/config/main-local.php',
 			'frontend/config/main-local.php',
 		],
