f804fe37336a20f798ac5d90635e3af54037a58a
Pedro Plowman <p2opensource@gmail.com>
2017-04-03 17:36:12 +1000

    v0.0.2

---
+++ assets/tests/functional/AboutCest.php
@@ -0,0 +1,13 @@
+<?php
+namespace static\tests\functional;
+
+use static\tests\FunctionalTester;
+
+class AboutCest
+{
+    public function checkAbout(FunctionalTester $I)
+    {
+        $I->amOnRoute('site/about');
+        $I->see('About', 'h1');
+    }
+}
